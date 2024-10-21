12fc17be53a0ca0aa46f8d604844ecee89509fd1
g g <gabedotgutierrez@gmail.com>
2024-10-20 22:18:23 -0400

    started controllers, might sleep

---
+++ controllers/tracks.js
@@ -0,0 +1,7 @@
+import Track from "../models/track";
+import express from "express";
+import { Router } from "express";
+
+
+
+export default trackRouter
--- server.js
+++ server.js
@@ -8,15 +8,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//import controler
+import trackRouter from "./controllers/tracks";
+
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+app.use(cors())
 app.use(express.json());
-
+app.use(logger('dev'))
 // Routes go here
+app.use('/tracks', trackRouter)
+
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
