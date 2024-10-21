012ae76712df6c8cbec83400fa823e25e0c69c59
g g <gabedotgutierrez@gmail.com>
2024-10-20 21:52:24 -0400

    setupe

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+import mongoose from "mongoose";
 import express from "express";
-import logger from "morgan";
-import cors from "cors";
 import dotenv from "dotenv";
+import cors from "cors"
+import logger from "morgan"
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
